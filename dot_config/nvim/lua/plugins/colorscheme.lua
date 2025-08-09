654f8dd93b250c7c6a9ab4345811722a634cb7fb
Sylvain Castaing <castaing.sylvain@gmail.com>
2025-08-09 16:45:45 +0200

    feat: add dotfiles with chezmoi

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  { "catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000 },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/whitespaces.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "mcauley-penney/visual-whitespace.nvim",
+    opts = {
+      enabled = true,
+      highlight = {
+        link = "Edit",
+        default = true,
+      },
+    },
+  },
+}
