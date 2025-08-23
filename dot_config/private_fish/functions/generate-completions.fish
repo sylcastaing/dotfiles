76f9714bedcfbbbf8befa41cc7dcff97e0979de7
Sylvain Castaing <castaing.sylvain@gmail.com>
2025-08-23 13:46:31 +0200

    feat: completion add script

---
+++ dot_config/private_fish/functions/generate-completions.fish
@@ -0,0 +1,13 @@
+function generate-completions --description "Generate fish completions for specified commands"
+    switch $argv[1]
+        case docker
+            docker completion fish >~/.config/fish/completions/docker.fish
+            echo "Docker completions generated and saved to ~/.config/fish/completions/docker.fish"
+        case volta
+            volta completions fish >~/.config/fish/completions/volta.fish
+            echo "Volta completions generated and saved to ~/.config/fish/completions/volta.fish"
+        case '*'
+            echo "Unknown command: $argv[1]"
+            echo "Usage: fish <docker|volta>"
+    end
+end
