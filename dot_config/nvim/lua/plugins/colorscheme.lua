d76f781bc94e8935e6cf9e6e24ae02cdcd5c7ee3
Sylvain Castaing <castaing.sylvain@gmail.com>
2025-08-25 19:59:42 +0200

    fix(nvim): catppuccin theme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,59 @@
 return {
-  { "catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000 },
-
   {
-    "LazyVim/LazyVim",
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        fzf = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        snacks = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        -- https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735
+        init = function()
+          local bufline = require("catppuccin.groups.integrations.bufferline")
+          function bufline.get()
+            return bufline.get_theme()
+          end
+        end,
+      },
     },
   },
 }
